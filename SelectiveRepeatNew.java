77b7b11821f1185f0b1ea2f5d6ae9dda5042baee
Scorpiussmith13 <sourasishdas18@yahoo.com>
2022-06-01 11:19:05 +0530

    Final few changes before sem ends.01/06/2022

--- SelectiveRepeatNew.java
+++ SelectiveRepeatNew.java
@@ -1,44 +1,63 @@
-import java.util.*
+import java.util.*;
 import java.io.*;
 import java.lang.*;
 class Comms{
   int data;
-  Comms(int data){
-    this.data=data;
-  }
-public void Sender(){
-  LinkedList<Integer> ll = new LinkedList<>();
-  InsertNewNode(ll,data);
-  CallingReciever(data);
-  if(data==10){
-    ShowLL(ll);
-  }
+  static ArrayList<Integer> ll=new ArrayList<>(),ll2=new ArrayList<>();
+//  static ArrayList<Integer> ll2=new ArrayList<>();
+
+
+public void Sender()throws IOException{
+  int max=1000;
+  int min=100;
+
+    BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+  System.out.println("How many packets");
+  int no_of_packets= Integer.parseInt(br.readLine());
+  for(int i=1;i<=no_of_packets;i++){
+        int turn=(int)(Math.random()*max)+min;
+        data=i;
+        System.out.println("Sending packet no "+i);
+        InsertNewNode(data);
+        if(turn>500 && turn<=700){
+          CallingReciever(0);
+          continue;
+        }
+      CallingReciever(data);
+
 }
-public void InsertNewNode(LinkedList<Integer> ll,int data){
-  ll.push_back(data);
+ShowLL(ll,ll2);
 }
-private void CallingReciever( int data){
-    LinkedList<Integer> ll2= new LinkedList<>();
-    InsertNewNode(ll2,data);
-    if(data==10){
-      ShowLL(ll);
-    }
+public void InsertNewNode(int data){
+    ll.add(data);
 }
-public void ShowLL(LinkedList<Integer> ll){
-  for(int i=0;i<ll.size();i++){
-    System.out.print(ll.get(i)+" ");
+public void InsertNewRNode(int data){
+    ll2.add(data);
+}
+public void CallingReciever( int data){
+//  ArrayList<Integer> ll2= new ArrayList<Integer>();
+    InsertNewRNode(data);
+
+}
+public void ShowLL(ArrayList<Integer> ll,ArrayList<Integer> ll2){
+  for(int i=0;i<ll2.size();i++){
+      if(ll2.get(i)==0){
+        System.out.println("Sending NAK for Packet no"+(i+1));
+        System.out.println("Resending Packet no "+(i+1));
+      }
+
   }
 
 }
 }
 class SelectiveRepeatNew{
-  public static void main(String args[]){
-  BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+  public static void main(String args[])throws IOException{
+
   System.out.println("Sender. Welcome!");
-  for(int i=1;i<11;i++){
-    Comms sd=new Sender(i);
+
+    Comms sd=new Comms();
     sd.Sender();
 
-  }
+
 }
 }
